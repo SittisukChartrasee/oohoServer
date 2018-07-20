f307590984ea7e5abfccd63d4ca0fc27a710380a
Sittisuk <sittisuk@codefin.io>
2018-07-20 19:45:24 +0700

    add header to server

--- cor/init.php
+++ cor/init.php
@@ -2,4 +2,18 @@
 header("Access-Control-Allow-Origin: *");
 header("content-type: application/json");
 require_once 'lib/nusoap.php';
+
+$WSHeader = array(
+  "UsernameToken"=>array(
+      "Username"=>"YourUserName",
+      "Password"=>"YourPassword",
+  )
+);
+
 $client = new nusoap_client('https://oohoserver.000webhostapp.com/server/server.php?wsdl');
+
+$header[] = new SoapHeader("http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd", "Security", $WSHeader);
+
+$client->__setSoapHeaders($header);
+
+// $REsponse = $client->YourRequest();
